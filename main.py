b493729e7c8c7662a64c905a1b86b6f374197b46
Bojackxiang <batmandocode@gmail.com>
2018-05-19 14:38:54 +1000

    update

--- main.py
+++ main.py
@@ -255,8 +255,8 @@ delay_file = open(path, 'w')
 i = 0
 while i<100:
     delay, cumulative_res_time = submission("trace", './assets/arrival_1.txt','./assets/service_1.txt', 5, 5, i, 100)
-    cum_file.write(cumulative_res_time + '/n')
-    delay_file.write(delay + '/n')
+    cum_file.write(cumulative_res_time, '/n')
+    delay_file.write(delay, '/n')
     i += 1
 
 cum_file.close()
